f3f71b27e5e1360848bd7744a9881fcc323b7f26
juanco1424 <jdcamargouseche@gmail.com>
2023-10-18 14:36:38 -0500

    Estructura inicial de proyecto

---
+++ src/test/java/com/digitalHouse/beerClub/BeerClubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalHouse.beerClub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerClubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
