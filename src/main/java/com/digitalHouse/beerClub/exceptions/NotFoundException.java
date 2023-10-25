32ce0d3892f67fe9882dc18f1bc8c798e2d2c130
juanco1424 <jdcamargouseche@gmail.com>
2023-10-24 19:27:43 -0500

    Merge

--- src/main/java/com/digitalHouse/beerClub/exceptions/NotFoundException.java
+++ src/main/java/com/digitalHouse/beerClub/exceptions/NotFoundException.java
@@ -1,10 +1,6 @@
 package com.digitalHouse.beerClub.exceptions;
 
-<<<<<<< HEAD
-public class NotFoundException extends Exception {
-=======
 public class NotFoundException extends Exception{
->>>>>>> d495d3a55f4b7568650a19839750178fbb781077
     public NotFoundException(String message) {
         super(message);
     }
--- src/main/java/com/digitalHouse/beerClub/exceptions/ServiceException.java
+++ src/main/java/com/digitalHouse/beerClub/exceptions/ServiceException.java
@@ -1,10 +1,6 @@
 package com.digitalHouse.beerClub.exceptions;
 
-<<<<<<< HEAD
-public class ServiceException extends Exception {
-=======
 public class ServiceException extends Exception{
->>>>>>> d495d3a55f4b7568650a19839750178fbb781077
     public ServiceException(String message) {
         super(message);
     }
