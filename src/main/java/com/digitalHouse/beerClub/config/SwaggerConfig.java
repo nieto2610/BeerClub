1641c67ae34d24c589992839d90c2b94d1ad744e
juanco1424 <jdcamargouseche@gmail.com>
2023-11-01 19:40:41 -0500

    Se modifica el swagger para que se pueda autenticar para hacer uso de las apis protegidas

--- src/main/java/com/digitalHouse/beerClub/config/SwaggerConfig.java
+++ src/main/java/com/digitalHouse/beerClub/config/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.digitalHouse.beerClub.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,7 @@ import org.springframework.context.annotation.Configuration;
 @OpenAPIDefinition(
         info = @Info(
                 title = "Beer Club API",
-                version = "1.0.0",
+                version = "1.4.2",
                 description = "Aplicación desarrollada para el Proyecto Integrador II de la carrera Certified Tech Developer de Digital House.\n\nGrupo 4(" +
                         "- Belén Santochi" +
                         " - Camilo Martinez" +
@@ -35,9 +38,20 @@ import org.springframework.context.annotation.Configuration;
                 )
         )
 )
-public class SwaggerConfig {
+    public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI();
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .in(SecurityScheme.In.HEADER)
+                .name("Authorization");
+
+        SecurityRequirement securityItem = new SecurityRequirement().addList("bearerAuth");
+
+        return new OpenAPI()
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .addSecurityItem(securityItem);
     }
 }
