5af184ec2592fce7086ffd4305a2f69412db5292
juanco1424 <jdcamargouseche@gmail.com>
2023-11-19 13:29:49 -0500

    Se crea la api para el filtro de subscriptions

--- src/main/java/com/digitalHouse/beerClub/controller/FilterController.java
+++ src/main/java/com/digitalHouse/beerClub/controller/FilterController.java
@@ -4,6 +4,7 @@ package com.digitalHouse.beerClub.controller;
 import com.digitalHouse.beerClub.exceptions.NoDataFoundException;
 import com.digitalHouse.beerClub.model.dto.PaymentDTO;
 import com.digitalHouse.beerClub.model.dto.PaymentFilterDTO;
+import com.digitalHouse.beerClub.model.dto.SubscriptionDTO;
 import com.digitalHouse.beerClub.model.dto.UserDTO;
 import com.digitalHouse.beerClub.service.implement.FilterService;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @Tag(name = "Filters")
@@ -39,7 +41,7 @@ public class FilterController {
         if (users.isEmpty()) {
             throw new NoDataFoundException("Se realizo la búsqueda, pero no se encontraron datos que coincidan con el criterio del filtro");
         } else if ((dateOne != null && dateTwo != null)) {
-            throw NoDataFoundException.forDateRange(dateOne, dateTwo);
+            NoDataFoundException.forDateRange(dateOne, dateTwo);
         }
 
         return ResponseEntity.status(HttpStatus.OK).body(users);
@@ -47,22 +49,39 @@ public class FilterController {
 
 
     @GetMapping(value = "/payments/filterGlobalData")
-    public ResponseEntity<?> getFilterPaymentsGlobalData(
-            @RequestParam(required = false) String descripcion,
+    public ResponseEntity<?> getFilterPaymentsGlobalData (
+            @RequestParam(required = false) String description,
             @RequestParam(required = false) Double amountMin,
             @RequestParam(required = false) Double amountMax,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateOne,
             @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate dateTwo
     ) {
-        List<PaymentFilterDTO> payments = service.getFilteredPayments(descripcion, amountMin, amountMax, dateOne, dateTwo);
+        List<PaymentFilterDTO> payments = service.getFilteredPayments(description, amountMin, amountMax, dateOne, dateTwo);
         if (payments.isEmpty()) {
             throw new NoDataFoundException("Se realizo la búsqueda, pero no se encontraron datos que coincidan con el criterio del filtro");
         } else if ((dateOne != null && dateTwo!= null)) {
-            throw NoDataFoundException.forDateRange(dateOne, dateTwo);
+            NoDataFoundException.forDateRange(dateOne, dateTwo);
         } else if ((amountMax != null && amountMin != null)) {
-            throw NoDataFoundException.validateRangeValues();
+            NoDataFoundException.validateRangeValues(amountMax, amountMin);
         }
         return ResponseEntity.status(HttpStatus.OK).body(payments);
+    }
+
+    @GetMapping(value = "/subscriptions/filterGlobalData")
+    public ResponseEntity<?> getFilterSubscription(
+            @RequestParam(required = false) String description,
+            @RequestParam(required = false) Integer isRecommended,
+            @RequestParam(required = false) Integer isActive,
+            @RequestParam(required = false) Double amountMin,
+            @RequestParam(required = false) Double amountMax
+    ) {
+        List<SubscriptionDTO> SubscriptionDTO = service.getFilterSubscription(description, isRecommended,isActive, amountMin, amountMax);
+        if (SubscriptionDTO.isEmpty()) {
+            throw new NoDataFoundException("Se realizo la búsqueda, pero no se encontraron datos que coincidan con el criterio del filtro");
+        }else if ((amountMax != null && amountMin != null)) {
+            NoDataFoundException.validateRangeValues(amountMax, amountMin);
+        }
+        return ResponseEntity.status(HttpStatus.OK).body(SubscriptionDTO);
     }
 
 
